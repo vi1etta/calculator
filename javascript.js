a6bdf599e5a8c603796d01177f5e1b8769a860d1
vi1etta <violet.samko@gmail.com>
2025-04-22 16:45:58 +0300

    Add sounds for btns and complete logic

--- javascript.js
+++ javascript.js
@@ -1,28 +1,9 @@
 const mainContainer = document.querySelector(".btn-holder");
 mainContainer.addEventListener("click", populateValue);
-
+const successSound = document.querySelector("#successSound");
+const btnClickSound = document.querySelector("#btnClickSound");
+const btnReset = document.querySelector("#btnReset");
 const dispayText = document.querySelector("#dispayText");
-
-const videoWrapper = document.querySelector("#video-wrapper");
-const video = document.querySelector("#hover-video");
-
-const resetBtn = document.querySelector("#clearBtn");
-
-resetBtn.addEventListener("mouseenter", showVideo);
-resetBtn.addEventListener("mouseleave", hideVideo);
-
-// Function to show video
-function showVideo() {
-  videoWrapper.style.opacity = 1;
-  video.play();
-  console.log("Hi!");
-}
-// Function to hide video
-function hideVideo() {
-  videoWrapper.style.opacity = 0;
-  video.pause();
-  video.currentTime = 0;
-}
 
 let a = "";
 let b = "";
@@ -31,22 +12,47 @@ let sign = "";
 let aIsCompleted = false;
 let result;
 
+function playSound(targetID) {
+  if (targetID === "equal") {
+    successSound.currentTime = 0; // rewind to start
+    successSound.play();
+  }
+  if (targetID === "math" || targetID === "digit") {
+    btnClickSound.currentTime = 0; // rewind to start
+    btnClickSound.play();
+  }
+
+  if (targetID === "clearBtn") {
+    btnReset.currentTime = 0;
+    btnReset.play();
+  }
+}
+
 function populateValue(e) {
-  if (e.target.matches("#digit") && aIsCompleted === false) {
+  if (
+    e.target.matches("#digit") &&
+    aIsCompleted === false &&
+    result === undefined &&
+    sign === "" &&
+    b === ""
+  ) {
     a += e.target.textContent;
     dispayText.textContent = a;
     console.log(a);
+    playSound(e.target.id);
   }
   if (e.target.matches("#math")) {
     aIsCompleted = true;
     sign = e.target.textContent;
     dispayText.textContent = a + sign;
     console.log(sign);
+    playSound(e.target.id);
   }
   if (e.target.matches("#digit") && aIsCompleted === true && sign !== "") {
     b += e.target.textContent;
     dispayText.textContent = a + sign + b;
     console.log(a + sign + b);
+    playSound(e.target.id);
   }
   if (
     e.target.matches("#equal") &&
@@ -55,6 +61,11 @@ function populateValue(e) {
     b != ""
   ) {
     operate(a, sign, b);
+    playSound(e.target.id);
+  }
+
+  if (e.target.matches("#equal") && aIsCompleted === false) {
+    showSnackbar();
   }
 
   if (e.target.matches("#clearBtn")) {
@@ -64,6 +75,36 @@ function populateValue(e) {
     aIsCompleted = false;
     result;
     dispayText.textContent = "Put a number";
+    playSound(e.target.id);
+  }
+
+  //next code for round 2+
+  if (
+    e.target.matches("#digit") &&
+    result != undefined &&
+    sign === "" &&
+    b === "" &&
+    a === ""
+  ) {
+    a += e.target.textContent;
+    dispayText.textContent = a;
+
+    console.log("next round");
+    playSound(e.target.id);
+  }
+
+  if (
+    e.target.matches("#math") &&
+    a === "" &&
+    result != undefined &&
+    b === ""
+  ) {
+    a = result;
+    aIsCompleted = true;
+    sign = e.target.textContent;
+    dispayText.textContent = a + sign;
+    console.log(sign, "round with sum of result success");
+    playSound(e.target.id);
   }
 }
 
@@ -72,7 +113,7 @@ function operate(aValue, signValue, bValue) {
     case "÷":
       result = Number(aValue) / Number(bValue);
       dispayText.textContent = result;
-      a = result;
+      a = "";
       b = "";
       sign = "";
       break;
@@ -80,7 +121,7 @@ function operate(aValue, signValue, bValue) {
     case "+":
       result = Number(aValue) + Number(bValue);
       dispayText.textContent = result;
-      a = result;
+      a = "";
       b = "";
       sign = "";
       console.log(result);
@@ -88,7 +129,7 @@ function operate(aValue, signValue, bValue) {
     case "×":
       result = Number(aValue) * Number(bValue);
       dispayText.textContent = result;
-      a = result;
+      a = "";
       b = "";
       sign = "";
       console.log(result);
@@ -96,34 +137,19 @@ function operate(aValue, signValue, bValue) {
     case "-":
       result = Number(aValue) - Number(bValue);
       dispayText.textContent = result;
-      a = result;
+      a = "";
       b = "";
       sign = "";
       console.log(result);
       break;
   }
-  console.log("It's work!");
 }
-console.log(result);
 
-const successSound = document.querySelector("#successSound");
-const hoverSound = document.querySelector("#hoverSound");
-
-mainContainer.addEventListener("click", (e) => {
-  const successSounds = e.target.id;
-  const btnIdEqual = ["equal"];
-  if (btnIdEqual.includes(successSounds)) {
-    successSound.currentTime = 0; // rewind to start
-    successSound.play();
-    console.log("hi!")
-  }
-});
-
-mainContainer.addEventListener("mouseover", (e) => {
-  const btnClicked = e.target.id;
-  const btnId = ["digit", "math", "equal"];
-  if (btnId.includes(btnClicked)) {
-    hoverSound.currentTime = 0; // rewind to start
-    hoverSound.play();
-  }
-});
+function showSnackbar() {
+  const snackbar = document.querySelector("#snackbar");
+  snackbar.className = "show";
+  snackbar.textContent = "Choose sign!";
+  setTimeout(function () {
+    snackbar.className = snackbar.className.replace("show", "");
+  }, 3000);
+}
